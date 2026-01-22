1b67e882913925660e88176cf894f16ef2ca6567
solenecor <solene.corbard@gmail.com>
2026-01-22 08:57:41 +0100

    parallélisation du déploiement telnet

--- telnet.py
+++ telnet.py
@@ -1,47 +1,72 @@
 import json
 import telnetlib3
 import time
-
-# A MODIFIER : dossier source
-SOURCE_CFG_DIR = "configs" # Dossier où le script de génération a déposé les fichiers de config
+import asyncio
+from multiprocessing import Pool
 
 # Récupération des noms des dossiers où les configs doivent être placées
 # A MODIFIER : nom du fichier gns3
 with open('projet_GNS.gns3', 'r', encoding='utf-8') as f:
     data = json.load(f)
     
-ports = {}
+tasks_data = []
 for node in data['topology']['nodes']:
-    name = node['name']
-    port_number = node['console']
-    ports[name] = port_number
+	name = node['name']
+	port = node['console']
+	path = f"configs/i{name[1:]}_startup-config.cfg" # Dossier où le script de génération a déposé les fichiers de config
+	tasks_data.append((name, port, path))
 
+def wrapper(args):
+    """
+    Un wrapper est nécessaire car Pool.map ne prend qu'un seul argument.
+    On décompresse le tuple (name, port, config_file).
+	Exécute la boucle asynchrone pour UN routeur dans UN processus du pool
+	"""
+    return asyncio.run(deploiement_telnet(*args))
 
-def deploiement_telnet(router_name, port, config_file):
+async def deploiement_telnet(router_name, port, config_file):
     print(f"--- Connexion à {router_name} sur le port {port} ---")
-    
-    tn = telnetlib3.Telnet("127.0.0.1", port) # Connexion au routeur sur localhost (127.0.0.1)
-    
-  
-    time.sleep(1)   # On attend pour être sûres que la connexion soit établie
-    tn.write(b"\r\n") # Simule la touche "Entrée" pour réveiller la console, b signifie qu'on envoie des bytes
+    try:
+	    _, writer = await telnetlib3.open_connection("127.0.0.1", port) # Connexion au routeur sur localhost (127.0.0.1)
+	
+	    writer.write('\r\n') # Simule la touche "Entrée" pour réveiller la console
+	    await writer.drain() # FORCE l'envoi des données du buffer vers le réseau
+	    await asyncio.sleep(1)  # Attente d'une seconde pour que le routeur réponde
+	
+	    
+	    writer.write("conf t\r\n") # Passage en mode configuration
+	    await writer.drain()
+	    
+	    # Lecture du fichier .cfg généré et envoi ligne par ligne
+	    with open(config_file, 'r') as f:
+	        for line in f:
+	            writer.write(line + "\r\n")
+	            await writer.drain() 
+	            await asyncio.sleep(0.1) #  Délai pour ne pas saturer le buffer du routeur
+	
+	            
+	    # Sauvegarde et fin
+	    writer.write("end\r\n")
+	    writer.write("write memory\r\n\r\n") # double \r\n pour la confirmation
+	    writer.write("exit\r\n")
+	    await writer.drain()
+	    await asyncio.sleep(1)
+	    
+	    writer.close()
+	    print(f"Configuration de {router_name} terminée via Telnet")
+		return f"{router_name} OK"
+    except Exception as e:
+        print(f"Erreur sur {router_name}: {e}")
+        return f"{router_name} ERROR"
 
+		
+if __name__ == "__main__":
+	
+	with Pool(processes=20) as pool:
+        print("Lancement du déploiement")
+        
+        results = pool.map(wrapper, tasks_data)
     
-    tn.write(b"conf t\r\n") # Passage en mode configuration
-    
-    # Lecture du fichier .cfg généré et envoi ligne par ligne
-    with open(config_file, 'r') as f:
-        for line in f:
-	        if line != "!":
-	            tn.write(line.encode('ascii') + b"\r\n")
-	            time.sleep(0.1) # Délai pour ne pas saturer le routeur
-            
-    # Sauvegarde et fin
-    tn.write(b"end\r\n")
-    tn.write(b"write memory\r\n")
-    tn.write(b"exit\r\n")
-    
-    print(f"Configuration de {router_name} terminée via Telnet")
-
-for name, port_number in ports.items(): 
-		deploiement_telnet(name, port_number, f"{SOURCE_CFG_DIR}/i{name[1]}_startup-config.cfg")
+    print("Déploiement terminé.")
+	for res in results:
+		print(res)
