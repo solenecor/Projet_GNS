6969aac4ec03625fd5730944e4e2173cbb8da3b8
solenecor <solene.corbard@gmail.com>
2026-01-28 00:33:05 +0100

    telnet async derniere version mais qui marche pas, commentaires

--- final_conf/drag_and_drop_bot.py
+++ final_conf/drag_and_drop_bot.py
@@ -2,17 +2,17 @@ import json
 import os
 import shutil
 
-# importation génération config
+# importation du code pour générer les configs
 from generate_conf import main as generate_main 
 
-# --- CONFIGURATION ---
 INTENT_FILE = "intent_file_17_routers.json"
 GNS3_FILE = '17_routers.gns3'
 SOURCE_CFG_DIR = "configs"
 GNS3_PROJECT_ROOT = "" # a compléter si le script est pas à la racine du projet GNS3
 
+
 def run_drag_and_drop_bot():
-    # lance génération conf
+    # lance génération des configs
     print("Début de la génération des fichiers de configuration")
     generate_main(INTENT_FILE)
     
@@ -21,23 +21,24 @@ def run_drag_and_drop_bot():
         print(f"Erreur : Le fichier {GNS3_FILE} est introuvable.")
         return 
 
-    # charge fichier 
+    # charge fichier gns3
     with open(GNS3_FILE, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-    # dictionnaire vide pour stocker ID routeurs 
+    # dictionnaire vide pour stocker les ID routeurs 
     folders = {}
     
-    for node in data['topology']['nodes']: #parcours liste de noeauds 
+    for node in data['topology']['nodes']: #parcours liste de noeuds 
         name = node['name']       # récupère le nom du routeur 
         node_id = node['node_id'] # récupère l'UUID unique (ex: "550e8400-e29b...")
         folders[name] = node_id   # remplit dico
 
+    # pour chaque routeur
     for name, node_id in folders.items():
-        source_file = os.path.join(SOURCE_CFG_DIR, f"i{name[1:]}_startup-config.cfg") # Reconstruit le nom du fichier source généré
+        source_file = os.path.join(SOURCE_CFG_DIR, f"i{name[1:]}_startup-config.cfg") # Reconstruit le chemin du fichier source généré
 
-        if os.path.exists(source_file): # vérifie création des fichiers précédents 
-            # construit chemin destination spécifique à GNS3/Dynamips
+        if os.path.exists(source_file): # vérifie si les fichiers sont déjà présents 
+            # construit le chemin destination spécifique à GNS3/Dynamips
             target_path = os.path.join(
                 GNS3_PROJECT_ROOT, 
                 "project-files", 
@@ -58,7 +59,7 @@ def run_drag_and_drop_bot():
                 # capture toute erreur
                 print(f" Erreur pour {name} ({node_id}) : {e}")
         else:
-            # alerte routeur dans GNS3 mais pas de fichier .cfg correspondant
+            # alerte que le routeur n'a pas de fichier .cfg correspondant
             print(f" Fichier introuvable pour {name} ({source_file})")
 
 if __name__ == "__main__":
--- final_conf/telnet.py
+++ final_conf/telnet.py
@@ -1,23 +1,22 @@
 import json
 import telnetlib3
 import asyncio
-import os
 
-# importation génération config
+# importation du code pour générer les configs
 from generate_conf import main as generate_main 
 
 INTENT_FILE = "intent_file_17_routers.json"
 GNS3_FILE = '17_routers.gns3'
 
-
+# charge le fichier gns3
 with open(GNS3_FILE, 'r', encoding='utf-8') as f:
-    data = json.load(f)
+    data = json.load(f) 
     
-tasks_data = []
-for node in data['topology']['nodes']:
-	name = node['name']
-	port = node['console']
-	path = f"configs/i{name[1:]}_startup-config.cfg" # Dossier où le script de génération a déposé les fichiers de config
+tasks_data = [] # liste pour stocker les données utiles
+for node in data['topology']['nodes']: # le fichiers gns3 est sous la forme de liste de liste de noeuds
+	name = node['name'] # on récupère le nom,
+	port = node['console'] # le port associé
+	path = f"configs/i{name[1:]}_startup-config.cfg" # Chemin vers où le script de génération a déposé les fichiers de config
 	tasks_data.append((name, port, path))
 
 
@@ -25,51 +24,46 @@ async def deploiement_telnet(router_name, port, config_file):
 
     print(f"--- Connexion à {router_name} sur le port {port} ---")
     try:
-        _, writer = await telnetlib3.open_connection(
+        reader, writer = await telnetlib3.open_connection(
             host="127.0.0.1",
             port=port,
-            shell=None,
-            connect_minwait=1.0, # On laisse le temps à la nego de se calmer
-            connect_maxwait=1,
-            encoding='utf-8',
-            force_binary=False,
-            term='vt100') # on force un terminal très simple ('vt100') pour limiter les échanges IAC
-             
-        writer.write('\x03') # Equivalent à un Ctrl+C
-        writer.write('\r\n\r\n\r\n') # Simule la touche "Entrée" 3 fois pour réveiller la console
-        writer.write('no\r\n')
-        await writer.drain()
-        await asyncio.sleep(2) # il peut y avoir beaucoup de blabla après le no
-        writer.write('\x03') # On recoupe direct au cas où le 'no' a lancé une recherche DNS (s'il n'y a pas eu la demande yes/no)
-        await writer.drain() # FORCE l'envoi des données du buffer vers le réseau
+            shell=None, #
+            connect_minwait=1.0, # 
+            connect_maxwait=1, #
+            encoding=None,  # 
+            force_binary=True, # 
+            )
 
-        writer.write("enable\r\n") # Passage en enable
+        writer.write(b'no\r\n') # pour répondre à la quetsion "Would you like to enter the initial configuration dialog? [yes/no]:" qui peut apparaître au début
         await writer.drain()
         await asyncio.sleep(0.5)
-        writer.write("conf t\r\n") # Passage en mode configuration
-        writer.write("no ip domain-lookup\r\n") # évite les pb en cas de mauvaise entrée
+        
+        writer.write(b"enable\r\n") # Passage en enable
         await writer.drain()
+        await asyncio.sleep(0.1) # pour etre sur 
+        writer.write(b"conf t\r\n") # Passage en mode configuration
+        writer.write(b"no ip domain-lookup\r\n") # évite les pb en cas de mauvaise entrée
+        await writer.drain()
+        await asyncio.sleep(0.1)
 
         # Lecture du fichier .cfg généré et envoi ligne par ligne
-        with open(config_file, 'r', encoding='utf-8') as f:
+        with open(config_file, 'r') as f:
             for line in f:
                 clean_line = line.strip() # On enlève les espaces et sauts de ligne invisibles
-                if clean_line: # On n'envoie que si la ligne n'est pas vide
-                    writer.write(clean_line + "\r\n")
+                if clean_line: # On envoie que si la ligne n'est pas vide
+                    writer.write(clean_line.encode() + b"\r\n") # on oublie pas de simuler la touche entrée
                     await writer.drain()
                     await asyncio.sleep(0.1) # Délai pour ne pas saturer le buffer du routeur
 
 
                 
         # Sauvegarde et fin
-        writer.write("end\r\n")
-        writer.write("write memory\r\n\r\n") # double \r\n pour la confirmation
-        writer.write("exit\r\n")
+        writer.write(b"write memory\r\n\r\n") # double \r\n pour la confirmation
+        writer.write(b"exit\r\n")
         await writer.drain()
-        await asyncio.sleep(2)
-
         writer.close()
-        #print(f"Configuration de {router_name} terminée via Telnet")
+        await asyncio.sleep(5) # le temps que tout arrive bien au routeur
+
         return f"{router_name} OK"
 
     except Exception as e:
@@ -80,21 +74,23 @@ async def deploiement_telnet(router_name, port, config_file):
 
 async def main():
 
-    # lance génération conf
+    # lance génération des configs
     print("Début de la génération des fichiers de configuration")
     generate_main(INTENT_FILE)
     
    
-    # On crée une liste de tâches (coroutines)
+    # On crée une liste de tâches, 1 par routeur
     tasks = [deploiement_telnet(name, port, path) for name, port, path in tasks_data]
     
     print(f"Lancement du déploiement des routeurs")
-    # .gather lance TOUT en même temps et attend que tout soit fini
+    # .gather lance tout en même temps et attend que tout soit fini 
     results = await asyncio.gather(*tasks)
     
-    print("\n--- Génération et Déploiement terminé ---")
+    
     for res in results:
         print(res)
+
+    print("\n--- Génération et Déploiement terminé ---")
 
 if __name__ == "__main__":
     asyncio.run(main())
