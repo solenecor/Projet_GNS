316bb3f22db95923e0dd73442b8e097472642ebc
solenecor <solene.corbard@gmail.com>
2026-01-28 09:23:41 +0100

    vf telnet

--- final_conf/telnet.py
+++ final_conf/telnet.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import json
 import telnetlib3.telnetlib as telnetlib
 import time
@@ -19,10 +21,10 @@ def deploiement_telnet(data):
     try:
         tn = telnetlib.Telnet("127.0.0.1", port) # Connexion au routeur sur localhost (127.0.0.1)
 
-        index, _, _ = tn.expect([b"yes/no]:", b"Router>", b"Press RETURN", b"console by console"], timeout=60) # on attend que le routeur soit prêt, chacune de ces options indique qu'il attend une action
+        index, _, _ = tn.expect([b"yes/no]:", b"Router>", b"Press RETURN", b"console by console", b"#"], timeout=60) # on attend que le routeur soit prêt au cas où qu'il ne le soit pas (chacune de ces options indique qu'il attend une action)
 
-        if index == 0:
-            tn.write(b'no\r\n') # pour répondre à la question "Would you like to enter the initial configuration dialog? [yes/no]:" qui peut apparaître au début
+        if index == 0: # si c'est la quetsion "Would you like to enter the initial configuration dialog? [yes/no]:" qui peut apparaître au début
+            tn.write(b'no\r\n') # on répond non à la question 
             time.sleep(0.1)
 
         tn.write(b"\r\n") # Simule la touche "Entrée" pour réveiller la console, b signifie qu'on envoie des bytes
@@ -37,6 +39,8 @@ def deploiement_telnet(data):
 
         tn.write(b"line con 0\r\nlogging synchronous\r\nexit\r\n") # On fait en sorte que le blabla de la console ne nous coupe pas au milieu de notre config quand une commande est en train d'etre ecrite
 
+        time.sleep(0.1)
+
         tn.write(b"no ip domain-lookup\r\n")  # On désactive la recherche DNS pour éviter les blocages notamment en cas d'instruction envoyée alors qu'on est pas au bon endroit (> au lieu de # par ex)
         
         # Lecture du fichier .cfg généré et envoi ligne par ligne
@@ -45,14 +49,17 @@ def deploiement_telnet(data):
                 clean_line = line.strip() # On enlève les espaces et sauts de ligne invisibles
                 if clean_line: # On envoie que si la ligne n'est pas vide
                     tn.write(line.encode('ascii') + b"\r\n")
-                    time.sleep(0.1) # Délai pour ne pas saturer le routeur
+                    tn.read_until(b"#") # pour attendre que le routeur soit prêt avant de continuer
                 
         # Sauvegarde et fin
         tn.write(b"write memory\r\n\r\n") # double \r\n pour la confirmation
         time.sleep(0.3)
         tn.write(b"exit\r\n")
 
-        return f"{router_name} OK"
+        tn.read_until(b"OK]") # attendre que ce soit bien enregistré
+
+        print(f"{router_name} OK")
+        return ""
 
     except Exception as e:
         print(f"Erreur sur {router_name}: {e}")
@@ -80,7 +87,5 @@ if __name__ == "__main__":
     with Pool(processes=20) as pool:
         results = pool.map(deploiement_telnet, tasks_data)
 
-    for res in results:
-        print(res)
     
     print("\n--- Génération et Déploiement terminé ---")
